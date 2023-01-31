926074d982d4d31f091a961bb134ab14b345b0c0
Charankasa07 <s180067@rguktsklm.ac.in>
2023-01-31 21:33:37 +0530

    accessing method mail has been changed

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ const {user_validation,login} = require('./validations/user_validation')
 
 const {task_validation} = require('./validations/task')
 
+var user_email;
+
 app.set('view engine','ejs');
 
 app.use(express.static('public'))
@@ -51,8 +53,8 @@ app.post('/register',async (req,res)=>{
     try {
         await post.save()
         app.set('email',req.body.email)
-        const dataemail = req.body.email
-        res.redirect(`/tasks/${dataemail}`)
+        user_email = req.body.email
+        res.redirect(`/tasks/${user_email}`)
     } catch (error) {
         res.send(error)
     }
@@ -69,8 +71,10 @@ app.post('/login',async (req,res)=>{
     const validpass = await bcrypt.compare(req.body.password,data.password)
     if(!validpass) return res.status(400).send("Invalid Password")
     
-    app.set('email',req.body.email)
-    res.redirect(`/tasks/${dataemail}`)
+    user_email = req.body.email
+
+    // app.set('email',req.body.email)
+    res.redirect(`/tasks/${user_email}`)
 })
 
 app.get('/tasks/:email',async (req,res)=>{
@@ -84,10 +88,8 @@ app.post('/add-task',async (req,res)=>{
     const {error,value} = task_validation(req.body)
     if(error) return res.status(400).send(error.details[0].message)
 
-    const email = app.get('email')
-
     const obj = new task({
-        email,
+        email:user_email,
         task:req.body.task,
         priority:req.body.priority,
         date:req.body.date
@@ -105,28 +107,28 @@ app.post('/add-task',async (req,res)=>{
 app.get('/delete-task/:id',async (req,res)=>{
     const id = req.params.id
     await task.findByIdAndUpdate(id,{$set :{status:"deleted"}},{new:true})
-    const email = app.get('email')
-    const tasks = await task.find({email}).sort({status:-1})
-    app.set('email',email)
+    // const email = app.get('email')
+    const tasks = await task.find({email:user_email}).sort({status:-1})
+    // app.set('email',email)
     res.render('main',{details:tasks})
 })
 
 app.get('/complete-task/:id',async (req,res)=>{
     const id = req.params.id
     await task.findOneAndUpdate({_id:id},{$set : {status:'completed'}},{new:true})
-    const email = app.get('email')
-    const tasks = await task.find({email}).sort({status:-1})
+    // const email = app.get('email')
+    const tasks = await task.find({email:user_email}).sort({status:-1})
     console.log(tasks);
-    app.set('email',email)
+    // app.set('email',email)
     res.render('main',{details:tasks})
 })
 
 app.get('/cancel-task/:id',async (req,res)=>{
     const id = req.params.id
     await task.findByIdAndUpdate(id,{$set :{status:"cancelled"}},{new:true})
-    const email = app.get('email')
-    const tasks = await task.find({email}).sort({status:-1})
-    app.set('email',email)
+    // const email = app.get('email')
+    const tasks = await task.find({email:user_email}).sort({status:-1})
+    // app.set('email',email)
     res.render('main',{details:tasks})
 })
 
